d8b322aae7aa2bac7fefd5e5ae2185dffd0a02a8
jtw023 <jordanwalters023@gmail.com>
2020-07-18 13:20:59 -0700

    fixed heroku typo

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // Defined paths for express config
 const publicDirectoryPath =
