fb5206eee1d3a73b6d12a26582e5745c0f10ae66
jtw023 <jordanwalters023@gmail.com>
2020-07-19 10:05:07 -0700

    fixing heroku typo

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(express.static(publicDirectoryPath));
 
 // What to show for the home page
 app.get('', (req, res) => {
-    res.render('index', {
+    res.render('/index', {
         title: 'Weather App',
         name: 'Jordan Walters',
     });
